93684530db316fa9272b36bbd7db7a91f28c34cb
Sheri Wilson <smgwilson37@gmail.com>
2016-09-05 16:35:30 -0700

    refactor app to work with local storage

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -31,6 +31,25 @@ describe('Actions', () => {
     expect(res).toEqual(action);
   });
 
+  it('should generate add todos action object', () => {
+    var todos = [
+      {
+        id: 111,
+        text: "Anything",
+        completed: false,
+        completedAt: undefined,
+        createdAt: 3300
+      }
+    ];
+    var action = {
+      type: 'ADD_TODOS',
+      todos
+    };
+    var res = actions.addTodos(todos);
+
+    expect(res).toEqual(action);
+  });
+
   it('should generate toggle todo action', () => {
     var action = {
       type: 'TOGGLE_TODO',
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -55,4 +55,25 @@ describe ('Todos reducer', () => {
     expect(res[0].completed).toEqual(false);
     expect(res[0].completedAt).toEqual(undefined);
   });
+
+  it('should add exisiting todos', () => {
+    var todos = [
+      {
+        id: 111,
+        text: "Anything",
+        completed: false,
+        completedAt: undefined,
+        createdAt: 3300
+      }
+    ];
+    var action = {
+      type: 'ADD_TODOS',
+      todos
+    };
+
+    var res = reducers.todosReducer(df([]), df(action));
+
+    expect(res.length).toEqual(1);
+    expect(res[0]).toEqual(todos[0]);
+  });
 });
