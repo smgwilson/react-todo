225bfddff27dc2c3dc858ea81f55a2f5802ac1d4
Sheri Wilson <smgwilson37@gmail.com>
2016-09-04 17:11:22 -0700

    adds showCompleted and searchText reducers and tests

---
+++ app/tests/reducers/reducers.test.jsx
@@ -0,0 +1,24 @@
+var expect = require('expect');
+var reducers = require('reducers');
+var df = require('deep-freeze-strict');
+
+describe ('Reducers', () => {
+  it ('should set searchText', () => {
+    var action = {
+      type: 'SET_SEARCH_TEXT',
+      searchText: 'dog'
+    };
+    var res = reducers.searchTextReducer(df(''), df(action));
+
+    expect(res).toEqual(action.searchText);
+  });
+
+  it ('should toggle showCompleted', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+    var res = reducers.showCompletedReducer(df(false), df(action));
+
+    expect(res).toEqual(true);
+  });
+});
